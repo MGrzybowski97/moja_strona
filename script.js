3ca25a62590e4a2d98e5902acd4eb806ef186bf2
Michał Grzybowski <mgrzybowski24@gmail.com>
2026-03-01 09:27:16 +0100

    Dodanie logiki w JavaScript

---
+++ script.js
@@ -0,0 +1,16 @@
+const items = document.querySelectorAll('li');
+
+items.forEach(item => {
+    item.addEventListener('click', () => {
+        
+        const text = item.innerText;
+
+        alert('Wybrałeś: ${text}. Powodzenia w nauce na Linux Mint!');
+
+        item.style.backgroundColor = '#ffcc00';
+        setTimeout(() => {
+            item.style.backgroundColor = '';
+        }, 500);
+
+    });
+});
